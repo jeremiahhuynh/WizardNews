fae4bb18edcb24a15d9552cc098056b9a5a11bf6
jeremiahhuynh <jeremiahhuynh890@gmail.com>
2023-04-17 17:13:17 -0400

    changes

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 const express = require("express");
 const app = express();
+const { list, find } = require("./postBank");
+const morgan = require("morgan");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  const posts = list();
+  res.send(`<html>
+  <head>
+    <link rel="stylesheet" href="/style.css" />
+    <title>Wizard News</title>
+  </head>
+  <body>
+    <h1>Wizard News</h1>
+    <ul>
+   ${posts
+     .map((post) => {
+       return `<li>${post.title}</li>`;
+     })
+     .join("")}
+
+    </ul>
+  </body>
+</html>`);
+res.send(html);
+});
 
 const PORT = 1337;
 
